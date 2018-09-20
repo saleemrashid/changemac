21492861cb953dd399d1149c33018b9125b6f995
Saleem Rashid <dev@saleemrashid.com>
2018-09-20 19:15:17 +0100

    addon.d: Fix SELinux context post-restore

--- addon.d/52-changemac.sh
+++ addon.d/52-changemac.sh
@@ -12,14 +12,14 @@ EOF
 case "$1" in
   backup)
     list_files | while read FILE DUMMY; do
-      backup_file $S/"$FILE"
+      backup_file "$S/$FILE"
     done
   ;;
   restore)
     list_files | while read FILE REPLACEMENT; do
       R=""
       [ -n "$REPLACEMENT" ] && R="$S/$REPLACEMENT"
-      [ -f "$C/$S/$FILE" ] && restore_file $S/"$FILE" "$R"
+      [ -f "$C/$S/$FILE" ] && restore_file "$S/$FILE" "$R"
     done
   ;;
   pre-backup)
@@ -32,6 +32,6 @@ case "$1" in
     # Stub
   ;;
   post-restore)
-    # Stub
+    chcon u:object_r:wcnss_service_exec:s0 "$S/xbin/changemac"
   ;;
 esac
